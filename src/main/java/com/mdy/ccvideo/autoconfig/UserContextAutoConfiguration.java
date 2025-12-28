ce83b52dbbc1fc8bae8d90c7ab010d441427ecbd
ddddyyyy <2115601599@qq.com>
2025-12-28 17:18:55 +0800

    兼容spring boot和非spring boot启动的情况下，环境变量的获取方式

--- src/main/java/com/mdy/ccvideo/autoconfig/UserContextAutoConfiguration.java
+++ src/main/java/com/mdy/ccvideo/autoconfig/UserContextAutoConfiguration.java
@@ -1,20 +1,32 @@
 package com.mdy.ccvideo.autoconfig;
 
+import com.mdy.ccvideo.dict.THQSConstants;
 import com.mdy.ccvideo.entity.UserContext;
+import com.mdy.ccvideo.util.EnvPropertyUtil;
 import com.mdy.ccvideo.util.UserContextHelper;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @ConditionalOnProperty(prefix = "ccvideo", name = "switch", havingValue = "true")
 public class UserContextAutoConfiguration {
     private static final Logger log = LogManager.getLogger(UserContextAutoConfiguration.class);
 
+    @Autowired
+    Environment environment;
+
     @Bean
     public UserContext userContext() {
+        log.info("init CCVIDEO UserContext by spring boot");
+        EnvPropertyUtil.set(THQSConstants.CC_VIDEO_SWITCH, environment.getProperty(THQSConstants.CC_VIDEO_SWITCH));
+        EnvPropertyUtil.set(THQSConstants.KEY_PROP, environment.getProperty(THQSConstants.KEY_PROP));
+        EnvPropertyUtil.set(THQSConstants.ROOM_KEY_PROP, environment.getProperty(THQSConstants.ROOM_KEY_PROP));
+        EnvPropertyUtil.set(THQSConstants.USERID_PROP, environment.getProperty(THQSConstants.USERID_PROP));
         return UserContextHelper.getInstance();
     }
 }
--- src/main/java/com/mdy/ccvideo/util/EnvPropertyUtil.java
+++ src/main/java/com/mdy/ccvideo/util/EnvPropertyUtil.java
@@ -1,6 +1,11 @@
 package com.mdy.ccvideo.util;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class EnvPropertyUtil {
+
+    private static final Logger log = LogManager.getLogger(EnvPropertyUtil.class);
 
     /**
      * 依次从 JVM 启动参数和环境变量获取值
@@ -14,5 +19,13 @@ public class EnvPropertyUtil {
             value = System.getenv(key);
         }
         return value;
+    }
+
+    /**
+     * SpringBoot启动的话，使用这个类把application里的内容设置进去
+     */
+    public static void set(String key, String value) {
+        log.info("Set env property: {}={}", key, value);
+        System.setProperty(key, value);
     }
 }
